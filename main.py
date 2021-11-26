c199c07a830fc74f021b5fd5be907462ea54a37e
christianyurianja <chris37fx@gmail.com>
2021-11-26 11:14:25 +0700

    MODULARISASI DENGAN FUNCTION

--- main.py
+++ main.py
@@ -6,29 +6,37 @@ MODULARISASI DENGAN FUNCTION
 
 def ekstraksi_data():
     """
-    Tanggal 12 Oktober 2021
-    Waktu: 05:23:31 WIB
-    Magnitudo: 3.9
-    Kedalaman: 29 km
-    Lokasi: LS=7.85 BT=107.28
-    Pusat Gempa: Pusat gempa berada di laut 96 km baratdaya Bandung
-    Dirasakan: Dirasakan (Skala MMI): II-III Garut
+    Tanggal: 25 November 2021
+    Waktu: 00:10:30 WIB
+    Magnitudo: 2.8
+    Kedalaman: 11 km
+    Lokasi: LS: 7.30 BT: 110.41
+    Pusat Gempa: Pusat gempa berada di darat 10 Km Barat Laut Kota Salatiga
+    Dirasakan: Dirasakan (Skala MMI): II Banyubiu, II Ambarawa, II Temenggungan,
+    II Pojoksari, II Brongkol, II Kalipawon, II Tegalrejo,
+    II Jambu, II Losari, II Gondorio, II Semilir, II Garung, II Bejalen
+    :return:
     """
-    hasil = dict()
-    hasil['tanggal'] = '12 Oktober 2021'
-    hasil['waktu'] = '05:23:31 WIB'
-    hasil['magnitudo'] = 3.9
-    hasil['lokasi'] = {'ls':7.85, 'bt':107.28}
-    hasil['pusat'] = 'Pusat gempa berada di laut 96 km baratdaya Bandung'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II-III Garut' 
+    hasil =  dict()
+    hasil['tanggal'] = '25 November 2021'
+    hasil['waktu'] = '00:10:30 WIB'
+    hasil['magnitudo'] = 2.8
+    hasil['kedalaman'] = '11 km'
+    hasil['lokasi'] = {'ls': 7.30, 'bt': 110.41}
+    hasil['pusat'] = 'Pusat gempa berada di darat 10 Km Barat Laut Kota Salatiga'
+    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II Banyubiu, II Ambarawa, II Temenggungan,' \
+                         ' II Pojoksari, II Brongkol, II Kalipawon, II Tegalrejo,' \
+                         'II Jambu, II Losari, II Gondorio, II Semilir, II Garung, II Bejalen'
 
     return hasil
 
+
 def tampilkan_data(result):
-    print('Gempa Terakhir berdasarkan BMKG')
+    print('Gempa terakhir berdasarkan BMKG')
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
     print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
     print(f"Lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
     print(f"Pusat {result['pusat']}")
     print(f"Dirasakan {result['dirasakan']}")
